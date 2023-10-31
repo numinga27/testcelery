9bbafa6412652504f958ffeb4673980033c244a6
numinga27 <numinga27@yandex.ru>
2023-10-31 22:05:13 +0400

    hockey

--- testcelery/testing/task.py
+++ testcelery/testing/task.py
@@ -41,8 +41,8 @@ def send_request(bind=True, autoretry_for=(RequestException,), retry_backoff=Tru
     result = []
     try:
 
-        # Tournament.objects.all().select_for_update().delete()
-        # Events.objects.all().select_for_update().delete()
+        Tournament.objects.all().select_for_update().delete()
+        Events.objects.all().select_for_update().delete()
         url = "https://fs.nimbase.cc/v1/events/live-list"
         headers = {
             'api-key-bravo': 'Nc4znHJeSs06G99YMVVBovHF',
